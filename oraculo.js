297aed210a69e1621939de2404f346ca7b218a6b
Gabriel Zanin <58155315+gabriel-zanin@users.noreply.github.com>
2020-12-03 14:22:34 -0300

    a
    a

--- oraculo.js
+++ oraculo.js
@@ -1,12 +1,248 @@
-var audio = new Audio(" audio_5.mp3")
+var audio1 = new Audio("audiofinalizado\\audio1.mp3");
+var audio2 = new Audio("audiofinalizado\\audio2.mp3");
+var audio3 = new Audio("audiofinalizado\\audio3.mp3");
+var audio4 = new Audio("audiofinalizado\\audio4.mp3");
+var audio5 = new Audio("audiofinalizado\\audio5.mp3");
+var audio6 = new Audio("audiofinalizado\\audio6.mp3");
+var audio7 = new Audio("audiofinalizado\\audio7.mp3");
+var audio8 = new Audio("audiofinalizado\\audio8.mp3");
+var audio9 = new Audio("audiofinalizado\\audio9.mp3");
+var audio10 = new Audio("audiofinalizado\\audio10.mp3");
+var audio11 = new Audio("audiofinalizado\\audio11.mp3");
+var audio12 = new Audio("audiofinalizado\\audio12.mp3");
+var audio13 = new Audio("audiofinalizado\\audio13.mp3");
+var audio14 = new Audio("audiofinalizado\\audio14.mp3");
+var audio15 = new Audio("audiofinalizado\\audio15.mp3");
+var audio16 = new Audio("audiofinalizado\\audio16.mp3");
+var audio17 = new Audio("audiofinalizado\\audio17.mp3");
+var audio18 = new Audio("audiofinalizado\\audio18.mp3");
+var audio19 = new Audio("audiofinalizado\\audio19.mp3");
+var audio20 = new Audio("audiofinalizado\\audio20.mp3");
+var audio21 = new Audio("audiofinalizado\\audio21.mp3");
+var audio22 = new Audio("audiofinalizado\\audio22.mp3");
+var audio23 = new Audio("audiofinalizado\\audio23.mp3");
+var audio24 = new Audio("audiofinalizado\\audio24.mp3");
+var audio25 = new Audio("audiofinalizado\\audio25.mp3");
+var audio26 = new Audio("audiofinalizado\\audio26.mp3");
+var audio27 = new Audio("audiofinalizado\\audio27.mp3");
 
-function clickIn() {
-    document.getElementById("teste").style.color = "blue";
-    audio.play();
+function inBtn1() {
+    audio1.play();
 }
 
-function ClickLeave() {
-    document.getElementById("teste").style.color = "green";
-    audio.pause();
+function outBtn1() {
+    audio1.pause();
+    audio1.currentTime = 0;
 }
+
+function inBtn2() {
+    audio2.play();
+}
+
+function outBtn2() {
+    audio2.pause();
+    audio2.currentTime = 0;
+}
+
+function inBtn3() {
+    audio3.play();
+}
+
+function outBtn3() {
+    audio3.pause();
+    audio3.currentTime = 0;
+}
+function inBtn4() {
+    audio4.play();
+}
+
+function outBtn4() {
+    audio4.pause();
+    audio4.currentTime = 0;
+}
+function inBtn5() {
+    audio5.play();
+}
+
+function outBtn5() {
+    audio5.pause();
+    audio5.currentTime = 0;
+}
+function inBtn6() {
+    audio6.play();
+}
+
+function outBtn6() {
+    audio6.pause();
+    audio6.currentTime = 0;
+}
+function inBtn7() {
+    audio7.play();
+}
+
+function outBtn7() {
+    audio7.pause();
+    audio7.currentTime = 0;
+}
+function inBtn8() {
+    audio8.play();
+}
+
+function outBtn8() {
+    audio8.pause();
+    audio8.currentTime = 0;
+}
+function inBtn9() {
+    audio9.play();
+}
+
+function outBtn9() {
+    audio9.pause();
+    audio9.currentTime = 0;
+}
+function inBtn10() {
+    audio10.play();
+}
+
+function outBtn10() {
+    audio10.pause();
+    audio10.currentTime = 0;
+}
+function inBtn11() {
+    audio11.play();
+}
+
+function outBtn11() {
+    audio11.pause();
+    audio11.currentTime = 0;
+}
+function inBtn12() {
+    audio12.play();
+}
+
+function outBtn12() {
+    audio12.pause();
+    audio12.currentTime = 0;
+}
+function inBtn13() {
+    audio13.play();
+}
+
+function outBtn13() {
+    audio13.pause();
+    audio13.currentTime = 0;
+}
+function inBtn14() {
+    audio14.play();
+}
+
+function outBtn14() {
+    audio14.pause();
+    audio14.currentTime = 0;
+}
+function inBtn15() {
+    audio15.play();
+}
+
+function outBtn15() {
+    audio15.pause();
+    audio15.currentTime = 0;
+}
+function inBtn16() {
+    audio16.play();
+}
+
+function outBtn16() {
+    audio16.pause();
+    audio16.currentTime = 0;
+}
+function inBtn17() {
+    audio17.play();
+}
+
+function outBtn17() {
+    audio17.pause();
+    audio17.currentTime = 0;
+}
+function inBtn18() {
+    audio18.play();
+}
+
+function outBtn18() {
+    audio18.pause();
+    audio18.currentTime = 0;
+}
+function inBtn19() {
+    audio19.play();
+}
+
+function outBtn19() {
+    audio19.pause();
+    audio19.currentTime = 0;
+}
+function inBtn20() {
+    audio20.play();
+}
+
+function outBtn20() {
+    audio20.pause();
+    audio20.currentTime = 0;
+}
+function inBtn21() {
+    audio21.play();
+}
+
+function outBtn21() {
+    audio21.pause();
+    audio21.currentTime = 0;
+}
+function inBtn22() {
+    audio22.play();
+}
+
+function outBtn22() {
+    audio22.pause();
+    audio22.currentTime = 0;
+}
+function inBtn23() {
+    audio23.play();
+}
+
+function outBtn23() {
+    audio23.pause();
+    audio23.currentTime = 0;
+}
+function inBtn24() {
+    audio24.play();
+}
+
+function outBtn24() {
+    audio24.pause();
+    audio24.currentTime = 0;
+}
+function inBtn25() {
+    audio25.play();
+}
+
+function outBtn25() {
+    audio25.pause();
+    audio25.currentTime = 0;
+}
+function inBtn26() {
+    audio26.play();
+}
+
+function outBtn26() {
+    audio26.pause();
+    audio26.currentTime = 0;
+}
+function inBtn27() {
+    audio27.play();
+}
+
+function outBtn27() {
+    audio27.pause();
+    audio27.currentTime = 0;
+}
+
 
